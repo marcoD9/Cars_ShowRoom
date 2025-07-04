c357516620a3019be2de063802b0ad9b902be9ec
Marco <marco.dmail96@gmail.com>
2025-07-04 14:43:10 +0200

    -Client- Update HomeView, add CarsComponent

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home">
+    <p>Home page</p>
+    <CarsComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CarsComponent from '../components/CarsComponent.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    CarsComponent,
+  },
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.home {
+  min-height: 100vh;
+}
+</style>
